f20b7587640382e69dcad18f6cf35f9ef320a55c
Igor Nikiforov <kolobamanacas@outlook.com>
2021-12-29 18:32:54 +0300

    Section 8 done

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { setTheme } from 'ngx-bootstrap/utils';
+import { IPagination } from 'src/models/pagination';
+import { IProduct } from 'src/models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[] = [];
+
+  constructor(private http: HttpClient) {
+    setTheme('bs4');
+  }
+
+  ngOnInit(): void {
+    // TODO: Remove any!
+    this.http.get<IPagination>('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
+      }, (error) => {
+        console.log(`Can't get data from https://localhost:5001/api/products. Error is: ${error}`);
+      });
+  }
 }
