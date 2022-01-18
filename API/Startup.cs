395c31222755ece975ef4cdb8d4ae30008578fc1
Igor Nikiforov <kolobamanacas@outlook.com>
2022-01-18 08:39:52 +0300

    Section 13 done

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using API.Helpers;
 using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -20,6 +21,11 @@ namespace API
         {
             services.AddControllers();
             services.AddDbContext<StoreContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgreSQLLocal")));
+            services.AddSingleton<IConnectionMultiplexer>((localConfig) =>
+            {
+                ConfigurationOptions config = ConfigurationOptions.Parse(configuration.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(config);
+            });
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
---
+++ Infrastructure/Data/BasketRepository.cs
@@ -0,0 +1,41 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Infrastructure.Data;
+
+public class BasketRepository : IBasketRepository
+{
+    private readonly IDatabase database;
+
+    public BasketRepository(IConnectionMultiplexer redis)
+    {
+        database = redis.GetDatabase();
+
+    }
+
+    public async Task<CustomerBasket?> GetBasketAsync(string basketId)
+    {
+        RedisValue value = await database.StringGetAsync(basketId);
+        return value.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(value);
+    }
+
+    public async Task<CustomerBasket?> UpdateBasketAsync(CustomerBasket basket)
+    {
+        string serializedBasket = JsonSerializer.Serialize(basket);
+        bool isCreated = await database.StringSetAsync(basket.Id, serializedBasket, TimeSpan.FromDays(30));
+
+        if (!isCreated)
+        {
+            return null;
+        }
+
+        return await GetBasketAsync(basket.Id);
+    }
+
+    public async Task<bool> DeleteBasketAsync(string basketId)
+    {
+        return await database.KeyDeleteAsync(basketId);
+    }
+}
